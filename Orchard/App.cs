8e2c35f29e89e4d65ea51760be24342affa56c6b
Sinh Pham <phamansinh@gmail.com>
2014-06-11 00:00:27 -0600

    New app.cs?

---
+++ Orchard/App.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xamarin.Forms;
+using System.Reflection;
+using System.IO;
+using System.Diagnostics;
+
+namespace Orchard
+{
+    public class App
+    {
+        public static Page GetMainPage()
+        {	
+            var rootPage = new RootPage();
+
+            return rootPage;
+        }
+    }
+}
+
