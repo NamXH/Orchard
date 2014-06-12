5de09c2507106dd21c7d9496d0080858cf22d82a
Sinh Pham <phamansinh@gmail.com>
2014-06-11 23:12:39 -0600

    Help text prototype.

--- Orchard/Page/Step1Page.xaml.cs
+++ Orchard/Page/Step1Page.xaml.cs
@@ -1,24 +1,64 @@
 ﻿using System;
 using System.Collections.Generic;
 using Xamarin.Forms;
+using System.Diagnostics;
 
 namespace Orchard
-{	
-	public partial class Step1Page : ContentPage
-	{	
-		public Step1Page ()
-		{
-			InitializeComponent ();
+{
+    public partial class Step1Page : ContentPage
+    {
+        public Step1Page()
+        {
+            InitializeComponent();
 
             BindingContext = new Step1VM();
 
-//            _picker.Items.Add("a");
-//            _picker.Items.Add("b");
-//            _picker.Items.Add("c");
-//            _picker.Items.Add("d");
-//            _picker.Items.Add("e");
+            var tgr = new TapGestureRecognizer((view, obj) =>
+            {
 
-		}
-	}
+                _rLayout.Children.Remove(_helpSv);
+                if (_showingHelp)
+                {
+                    // Need to hide
+                    _rLayout.Children.Add(_helpSv,
+                        Constraint.Constant(0),
+                        Constraint.RelativeToParent((parent) =>
+                        {
+                            return parent.Height - 50;
+                        }),
+                        Constraint.RelativeToParent((parent) =>
+                        {
+                            return parent.Width;
+                        }),
+                        Constraint.Constant(50));
+                }
+                else
+                {
+                    // Need to show
+                    _rLayout.Children.Add(_helpSv,
+                        Constraint.Constant(0),
+                        Constraint.RelativeToParent((parent) =>
+                        {
+                            return parent.Height - 200;
+                        }),
+                        Constraint.RelativeToParent((parent) =>
+                        {
+                            return parent.Width;
+                        }),
+                        Constraint.Constant(200));
+                }
+                _showingHelp = !_showingHelp;
+                Debug.WriteLine("tapped");
+            });
+
+
+
+            _helpLb.GestureRecognizers.Add(tgr);
+
+
+        }
+
+        bool _showingHelp;
+    }
 }
 
