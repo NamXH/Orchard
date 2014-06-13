92204bbd504d83552e039cb175830b3899f789f4
Sinh Pham <phamansinh@gmail.com>
2014-06-13 15:40:03 -0600

    Step 1 page.

---
+++ Orchard/Page/Step1Page.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Orchard
+{
+    public partial class Step1Page : ContentPage
+    {
+        public Step1Page()
+        {
+            InitializeComponent();
+
+            var vm = new Step1VM();
+
+            BindingContext = vm;
+
+            ViewUtils.SetupStepView(_rLayout, _helpSv, _questionContainer, vm.Common.QuestionTapped);
+        }
+
+        public void NextClicked(object sender, EventArgs e)
+        {
+            MessagingCenter.Send((Page)this, "next");
+        }
+    }
+}
+
