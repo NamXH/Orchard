60ed03978a53368e957b24b282ff9044ef75866a
Sinh Pham <phamansinh@gmail.com>
2014-06-13 09:29:06 -0600

    Reading question data from file.

--- Orchard/Helper.cs
+++ Orchard/Helper.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Reflection;
 using System.IO;
+using System.Collections.Generic;
 
 namespace Orchard
 {
@@ -17,6 +18,24 @@ namespace Orchard
                 {
                     var str = sr.ReadToEnd();
                     return str;
+                }
+            }
+        }
+
+        public static IEnumerable<string> ReadTextDataToLine(string filename)
+        {
+            var assembly = typeof(App).GetTypeInfo().Assembly;
+            var resName = string.Format("Orchard.Data.{0}", filename);
+
+            using (var stream = assembly.GetManifestResourceStream(resName))
+            {
+                using (var sr = new StreamReader(stream))
+                {
+                    string str = null;
+                    while ((str = sr.ReadLine()) != null)
+                    {
+                        yield return str;
+                    }
                 }
             }
         }
