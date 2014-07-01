739bc7af62ed1d29f2318f7accd0d93bef402b88
Sinh Pham <phamansinh@gmail.com>
2014-06-30 23:30:27 -0600

    MyPicker prototype.

--- Orchard/Page/Step1Page.xaml.cs
+++ Orchard/Page/Step1Page.xaml.cs
@@ -24,5 +24,33 @@ namespace Orchard
             MessagingCenter.Send((Page)this, "next");
         }
     }
+
+    public class MyPicker : Layout<View>
+    {
+        public MyPicker()
+        {
+            IsClippedToBounds = true;
+        }
+
+        #region implemented abstract members of Layout
+
+        protected override void LayoutChildren(double x, double y, double width, double height)
+        {
+            var i = 0;
+            foreach (var c in Children)
+            {
+                Rectangle region = new Rectangle(i * width + x, y, width, height);
+                ++i;
+                Xamarin.Forms.Layout.LayoutChildIntoBoundingRegion(c, region);
+            }
+
+
+            Debug.WriteLine("lc: {0} {1} {2} {3}", x, y, width, height);
+        }
+
+        #endregion
+
+
+    }
 }
 
---
+++ iOS/Implementor/MyPickerRenderer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Xamarin.Forms.Platform.iOS;
+using MonoTouch.UIKit;
+using Xamarin.Forms;
+using Orchard;
+using Orchard.iOS;
+using MonoTouch.CoreGraphics;
+using System.Drawing;
+
+
+[assembly: ExportRenderer(typeof(MyPicker), typeof(MyPickerRenderer))]
+namespace Orchard.iOS
+{
+    public class MyPickerRenderer : ViewRenderer<MyPicker, UIScrollView>
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<MyPicker> e)
+        {
+            base.OnElementChanged(e);
+            if (e.OldElement == null)
+            {
+                // Initial setups.
+                var pickerView = new UIScrollView(new System.Drawing.RectangleF(0, 0, (float)e.NewElement.WidthRequest, (float)e.NewElement.HeightRequest));
+
+                SetNativeControl(pickerView);
+
+                Control.PagingEnabled = true;
+                Control.ShowsHorizontalScrollIndicator = false;
+                Control.AutoresizingMask = UIViewAutoresizing.FlexibleWidth | UIViewAutoresizing.FlexibleHeight;
+                Control.ClipsToBounds = true;
+           
+
+                Control.BackgroundColor = UIColor.LightGray;
+
+                foreach (var view in e.NewElement.Children)
+                {
+                    var renderer = RendererFactory.GetRenderer(view);
+                    Control.Add(renderer.NativeView);
+                }
+
+                Control.ContentSize = new SizeF(1000, 100);
+            }
+        }
+    }
+
+}
+
