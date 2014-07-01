32baadca34c50dcf3d9c976374d1950c9bb8753f
Sinh Pham <phamansinh@gmail.com>
2014-07-01 16:01:28 -0600

    Overlay for measure tool prototype.

--- iOS/Implementor/MeasureTool.cs
+++ iOS/Implementor/MeasureTool.cs
@@ -6,6 +6,7 @@ using Xamarin.Media;
 using System.Diagnostics;
 using MonoTouch.UIKit;
 using System.Linq;
+using MonoTouch.CoreGraphics;
 
 [assembly: Dependency(typeof(MeasureTool))]
 namespace Orchard.iOS
@@ -43,11 +44,27 @@ namespace Orchard.iOS
             picker.CameraDevice = UIImagePickerControllerCameraDevice.Rear;
             picker.ShowsCameraControls = false;
             picker.NavigationBarHidden = true;
+            //picker.PrefersStatusBarHidden();
             picker.ToolbarHidden = true;
 
+            var screenSize = UIScreen.MainScreen.Bounds.Size;
+            var cameraAspectRatio = 4.0 / 3.0;
+            var imgHeight = Math.Floor(screenSize.Width * cameraAspectRatio);
+            var scaleFactor = (float)Math.Ceiling(screenSize.Height / imgHeight);
 
-
+            picker.CameraViewTransform = CGAffineTransform.MakeScale(scaleFactor, scaleFactor);
             //picker.MediaTypes = UIImagePickerController.AvailableMediaTypes(UIImagePickerControllerSourceType.Camera);
+
+            //var overlayRect = CGRectMake(0, 0, self.imagePickerController.view.frame.size.width,      self.imagePickerController.view.frame.size.height);
+
+            var overlayView = new UIView(new System.Drawing.RectangleF(0, 0, picker.View.Frame.Width, picker.View.Frame.Height));
+
+            var unitSlider = new UISlider(new System.Drawing.RectangleF(-10, picker.View.Frame.Height - 50, 150, 30));
+            unitSlider.Layer.AnchorPoint = new System.Drawing.PointF(0, 0);
+            unitSlider.Transform = CGAffineTransform.MakeRotation((float)(-Math.PI / 2));
+
+            overlayView.AddSubview(unitSlider);
+            picker.CameraOverlayView = overlayView;
 
             uIViewController.PresentViewController(picker, true, null);
             return (double)0;
