052a42e8159f16de8bb3f48f8c3ac8efa0e395f6
Sinh Pham <phamansinh@gmail.com>
2014-07-30 16:47:28 -0600

    Change density images based on grow stage.

--- Orchard/ViewModel/Step2VM.cs
+++ Orchard/ViewModel/Step2VM.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.ObjectModel;
+using Xamarin.Forms;
 
 namespace Orchard
 {
@@ -7,6 +9,10 @@ namespace Orchard
         public Step2VM()
         {
             Common = new StepVMCommon("Step2Questions.txt", "Step2HelpTexts.txt", "Tree Shape and Density");
+
+            DenseImg = ImageSource.FromResource("Orchard.Images.Density.pre-dense.jpg");
+            OpenImg = ImageSource.FromResource("Orchard.Images.Density.pre-open.jpg");
+            ExtreOpenImg = ImageSource.FromResource("Orchard.Images.Density.pre-extreOpen.jpg");
         }
 
         public StepVMCommon Common{ get; set; }
@@ -16,7 +22,51 @@ namespace Orchard
         public GrowthStage CurrGrowStage
         {
             get { return _currGrowStage; }
-            set { SetProperty(ref _currGrowStage, value); }
+            set
+            {
+                SetProperty(ref _currGrowStage, value);
+                switch (value)
+                {
+                    case GrowthStage.PrePetaFall:
+                        {
+                            DenseImg = ImageSource.FromResource("Orchard.Images.Density.pre-dense.jpg");
+                            OpenImg = ImageSource.FromResource("Orchard.Images.Density.pre-open.jpg");
+                            ExtreOpenImg = ImageSource.FromResource("Orchard.Images.Density.pre-extreOpen.jpg");
+                        }
+                        break;
+                    case GrowthStage.PostPetaFall:
+                        {
+                            DenseImg = ImageSource.FromResource("Orchard.Images.Density.post-dense.jpg");
+                            OpenImg = ImageSource.FromResource("Orchard.Images.Density.post-open.jpg");
+                            ExtreOpenImg = ImageSource.FromResource("Orchard.Images.Density.post-extreOpen.jpg");
+                        }
+                        break;
+                }
+            }
+        }
+
+        ImageSource _denseImg;
+
+        public ImageSource DenseImg
+        {
+            get { return _denseImg; }
+            set { SetProperty(ref _denseImg, value); }
+        }
+
+        ImageSource _openImg;
+
+        public ImageSource OpenImg
+        {
+            get { return _openImg; }
+            set { SetProperty(ref _openImg, value); }
+        }
+
+        ImageSource _extreOpenImg;
+
+        public ImageSource ExtreOpenImg
+        {
+            get { return _extreOpenImg; }
+            set { SetProperty(ref _extreOpenImg, value); }
         }
 
         public enum GrowthStage
